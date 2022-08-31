fc579177914885aacb4989500c9eeb03cff4d2e1
Furkan Kenger <kengermf@gmail.com>
2022-08-31 20:16:29 +0300

    V3

---
+++ isNULL.sql
@@ -0,0 +1,54 @@
+-- IS NULL, IS NOT NULL, COALESCE --
+/*
+
+IS NULL ve IS NOT NULL Boolean operatorleridir. Bir ifadenin NULL olup olmadıgını kontrol eder.
+
+COALESCE (Birleştirme) ise bir fonksiyondur ve icerisindeki parametrelerden NULL olmayan ilk ifadeyi döndürür.
+
+*/
+
+CREATE TABLE insanlar(	
+ssn CHAR(9), -- Social Security Number	
+isim VARCHAR(50), 	
+adres VARCHAR(50) );
+INSERT INTO insanlar VALUES('123456789', 'Ali Can', 'Istanbul');
+INSERT INTO insanlar VALUES('234567890', 'Veli Cem', 'Ankara');
+INSERT INTO insanlar VALUES('345678901', 'Mine Bulut', 'Izmir');
+INSERT INTO insanlar (ssn, adres) VALUES('456789012', 'Bursa');
+INSERT INTO insanlar (ssn, adres) VALUES('567890123', 'Denizli');
+INSERT INTO insanlar (adres) VALUES('Sakarya');
+INSERT INTO insanlar (ssn) VALUES('999111222');
+
+select * from insanlar
+
+-- isim i null olanlari sorgula
+SELECT * from insanlar where isim is null
+
+-- null olmayanları listeleyelim
+SELECT * from insanlar WHERE isim is not null
+
+-- isim'i NULL olan kişilerin isim'ine NO NAME atayınız.
+UPDATE insanlar
+SET isim = 'NO NAME'
+WHERE isim is null
+
+-- Tabloyu bir önceki soruyu çözmeden önceki haline geri getirin
+UPDATE insanlar
+SET isim = null
+WHERE isim = 'NO NAME'
+
+--NOT: Çoklu degişimde her değişim için ifade yazmamak icin SET... WHERE isim NULL gibi
+--ifade yazmamak icin COALESCE(Birleştirme) kullanılır
+
+/* 
+isim 'i NULL olanlara 'Henuz isim girilmedi'
+adres 'i NULL olanlara 'Henuz adres girilmedi'
+ssn 'i NULL olanlara ' no ssn' atayalım.
+*/
+
+UPDATE insanlar
+SET isim = coalesce (isim, 'HENUZ ISIM GIRILMEDI'),
+    adres = coalesce (adres, 'HENUZ ADRES GIRILMEDI'),
+	ssn = coalesce (ssn, 'NO SSN');
+	
+select * from insanlar
